d96a7bf87fd7b227ecee720e2e483890ec5a65af
FlavioBS <bzsflavio@gmail.com>
2019-09-07 20:25:25 -0300

    estilos globais

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
+      {/* <Header/> */}
       <Routes />
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
