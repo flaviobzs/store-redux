53fd0dd7cb1d2c46c6370fd19c3ec010176db616
FlavioBS <bzsflavio@gmail.com>
2019-09-08 16:37:12 -0300

    remover produtos do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,8 +7,9 @@ import {
 } from 'react-icons/md';
 
 import { Container, ProductTable, Total } from './styles';
+import { dispatch } from '../../../../../AppData/Local/Microsoft/TypeScript/3.5/node_modules/rxjs/internal/observable/pairs';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -52,7 +53,15 @@ function Cart({ cart }) {
                 <strong>R$258,80</strong>
               </td>
               <td>
-                <button>
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({
+                      type: '@cart/DELETE',
+                      id: product.id,
+                    })
+                  }
+                >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,15 +1,31 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   // quando é realizado uma action, todo os reduces são chamados, por isso, utililar esse switch para indicar apenar o necessario
 
   switch (action.type) {
     case '@cart/ADD':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1, // inicializar contador com 1
-        },
-      ];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
+    case '@cart/DELETE':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+        // verificação se existe algum elemento no array
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
+
     default:
       return state;
   }
